a94fc2084dcceb2402dd1ab7109b7a1aa1ad6abd
offcn-caozhen <syunlongs@126.com>
2017-11-06 14:44:41 +0800

    练习上传项目到服务器端中央仓库！

---
+++ src/main/java/com/caozhen/Demo1.java
@@ -0,0 +1,111 @@
+package com.caozhen;
+
+import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
+
+public class Demo1 {
+
+	@Test
+	public void getExcel() throws FileNotFoundException, IOException{
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		XSSFSheet sheet = workbook.createSheet("工资表");
+		//指定合并开始行、结束行，开始列，结束列
+		CellRangeAddress range = new CellRangeAddress(0, 0, 0, 11);
+		//添加要合并的地址到单元格
+		sheet.addMergedRegion(range);
+		XSSFRow row = sheet.createRow(0);
+		XSSFCell cell = row.createCell(0);
+		cell.setCellValue("二O一五年五月工资表");
+		
+		CellRangeAddress range1 = new CellRangeAddress(1, 2, 0, 0);
+		sheet.addMergedRegion(range1);
+		XSSFRow row1 = sheet.createRow(1);
+		XSSFCell cell1 = row1.createCell(0);
+		cell1.setCellValue("部门");
+		
+		CellRangeAddress range2 = new CellRangeAddress(1, 2, 1, 1);
+		sheet.addMergedRegion(range2);
+		
+		XSSFCell cell2 = row1.createCell(1);
+		cell2.setCellValue("姓名");
+		
+		CellRangeAddress range3 = new CellRangeAddress(1, 1, 2, 3);
+		sheet.addMergedRegion(range3);		
+		XSSFCell cell3 = row1.createCell(2);
+		cell3.setCellValue("固定工资");
+		
+		CellRangeAddress range4 = new CellRangeAddress(1, 1, 4, 6);
+		sheet.addMergedRegion(range4);
+		XSSFCell cell4 = row1.createCell(4);
+		cell4.setCellValue("浮动工资");
+		
+		CellRangeAddress range5 = new CellRangeAddress(1, 2, 7, 7);
+		sheet.addMergedRegion(range5);
+		XSSFCell cell5 = row1.createCell(7);
+		cell5.setCellValue("请假/扣款");
+		
+		CellRangeAddress range6 = new CellRangeAddress(1, 2, 8, 8);
+		sheet.addMergedRegion(range6);
+		XSSFCell cell6 = row1.createCell(8);
+		cell6.setCellValue("应付工资总额");
+		
+		CellRangeAddress range7 = new CellRangeAddress(1, 1, 9, 10);
+		sheet.addMergedRegion(range7);
+		XSSFCell cell7 = row1.createCell(9);
+		cell7.setCellValue("代扣款项");
+		
+		CellRangeAddress range8 = new CellRangeAddress(1, 2, 11, 11);
+		sheet.addMergedRegion(range8);
+		XSSFCell cell8 = row1.createCell(11);
+		cell8.setCellValue("实发工资");
+		
+		XSSFRow row2 = sheet.createRow(2);
+		XSSFCell cell9 = row2.createCell(2);
+		cell9.setCellValue("基本工资");
+		XSSFCell cell10 = row2.createCell(3);
+		cell10.setCellValue("岗位工资");
+		XSSFCell cell11 = row2.createCell(4);
+		cell11.setCellValue("交通补贴");
+		XSSFCell cell12 = row2.createCell(5);
+		cell12.setCellValue("食宿补贴");
+		XSSFCell cell13 = row2.createCell(6);
+		cell13.setCellValue("加班工资");
+		
+		XSSFCell cell14 = row2.createCell(9);
+		cell14.setCellValue("社医保");
+		XSSFCell cell15 = row2.createCell(10);
+		cell15.setCellValue("公积金");
+		
+		String[]rows3 = {"行政","张三","1000.00","1400.00","100.00","","10.00","","2510.00","128.28","144.00","2237.72"};
+		XSSFRow row3 = sheet.createRow(3);
+		for(int i=0;i<rows3.length;i++){
+			row3.createCell(i).setCellValue(rows3[i]);	
+		}
+		CellRangeAddress range9 = new CellRangeAddress(9, 9, 0, 5);
+		sheet.addMergedRegion(range9);
+		XSSFRow row9 = sheet.createRow(9);
+		XSSFCell cell16 = row9.createCell(0);
+		cell16.setCellValue("备注：本人确认工资无误。               签名：     ");
+
+		XSSFCellStyle style = workbook.createCellStyle();
+		//居中
+		style.setAlignment(HorizontalAlignment.CENTER);
+		
+		//为指定单元格设定样式
+		cell.setCellStyle(style);
+		workbook.write(new FileOutputStream("D:\\A第五阶段\\photoDemo\\homework.xlsx"));
+		workbook.close();
+	}
+}
---
+++ src/main/java/com/caozhen/Demo2.java
@@ -0,0 +1,135 @@
+package com.caozhen;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.color.Color;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.TextAlignment;
+
+public class Demo2 {
+
+	@Test
+	public void getPdfDemo() throws IOException{
+		PdfWriter writer = new PdfWriter("D:\\A第五阶段\\photoDemo\\个人简历.pdf");
+		PdfDocument pdfdoc = new PdfDocument(writer);
+		Document doc = new Document(pdfdoc,PageSize.A4.rotate());
+		//设置页边距
+	    doc.setMargins(0, 20, 20, 20);
+				
+		//设定支持中文的字体，一定要导入亚洲字体包
+		PdfFont font = PdfFontFactory.createFont("STSong-Light", "UniGB-UCS2-H",false);
+		doc.add(new Paragraph("简        历").setFontSize(15).setTextAlignment(TextAlignment.CENTER).setFont(font));
+		Table table = new Table(new float[]{200,230,140,230,200});
+		Cell c1 = new Cell(1,5);
+		c1.add(new Paragraph("基本情况").setFont(font).setBackgroundColor(Color.LIGHT_GRAY));
+		table.addCell(c1);
+		Cell c7 = new Cell();
+		c7.add(new Paragraph("姓名").setFont(font));
+		table.addCell(c7);
+		Cell c8 = new Cell();
+		c8.add(new Paragraph().setFont(font).setBackgroundColor(Color.LIGHT_GRAY));
+		table.addCell(c8);
+		Cell c9 = new Cell();
+		c9.add(new Paragraph("性别").setFont(font));
+		table.addCell(c9);
+		Cell c10 = new Cell();
+		c10.add(new Paragraph().setFont(font).setBackgroundColor(Color.LIGHT_GRAY));
+		table.addCell(c10);
+		//图片
+		Cell c11 = new Cell(7,1).setTextAlignment(TextAlignment.CENTER);
+		Image img = new Image(ImageDataFactory.create("D:\\A第五阶段\\666.jpg"));
+		/*img.scaleAbsolute(134, 185);
+		img.setAutoScaleHeight(true);
+		img.setAutoScaleWidth(true);*/
+		c11.add(img);
+		table.addCell(c11);
+		String[]cells1={"民  族","","出生年月","","政治面貌","","健康状况","","籍  贯","","学  历","","电子信箱","","联系电话",""};
+		for(int i=1;i<=cells1.length;i++){
+		     table.addCell(new Cell().add(new Paragraph(cells1[i-1])).setFont(font));
+		}
+		Cell c2 = new Cell();
+		c2.add(new Paragraph("专业")).setFont(font);
+		table.addCell(c2);
+		Cell c3 = new Cell(1,3);
+		c3.add(new Paragraph("")).setFont(font);
+		table.addCell(c3);
+		Cell c4 = new Cell();
+		c4.add(new Paragraph("毕业院校")).setFont(font);
+		table.addCell(c4);
+		Cell c5 = new Cell(1,3);
+		c5.add(new Paragraph("")).setFont(font);
+		table.addCell(c5);
+		Cell c6 = new Cell();
+		c6.add(new Paragraph("求职意向")).setFont(font);
+		table.addCell(c6);
+		Cell c12 = new Cell(1,4);
+		c12.add(new Paragraph("")).setFont(font);
+		table.addCell(c12);
+		
+		//创建新表
+		Table table2 = new Table(new float[]{250,250,250,250});
+		Cell c13 = new Cell(1,4);
+		c13.add(new Paragraph("教育情况").setFont(font).setBackgroundColor(Color.LIGHT_GRAY));
+		table2.addCell(c13);
+		String[]cells2={"时间","培训机构","专业","学历"};
+		for(int i=1;i<=cells2.length;i++){
+			 table2.addCell(new Cell().add(new Paragraph(cells2[i-1])).setFont(font));
+		}
+		for(int i=0;i<8;i++) {
+			Cell cell=new Cell().setTextAlignment(TextAlignment.LEFT).setHeight(20F);
+			table2.addCell(cell);
+		}
+		Cell c14 = new Cell(1,4);
+		c14.add(new Paragraph("技能特长").setFont(font).setBackgroundColor(Color.LIGHT_GRAY));
+		table2.addCell(c14);
+		Cell c15 = new Cell(2,4).setTextAlignment(TextAlignment.LEFT).setHeight(40F);
+		
+		table2.addCell(c15);
+		Cell c16 = new Cell(1,4);
+		c16.add(new Paragraph("工作经验").setFont(font).setBackgroundColor(Color.LIGHT_GRAY));
+		table2.addCell(c16);
+		String[]cells3={"时间","公司名称","职位","工作内容"};
+		for(int i=1;i<=cells3.length;i++){
+			 table2.addCell(new Cell().add(new Paragraph(cells3[i-1])).setFont(font));
+		}
+		for(int i=0;i<4;i++) {
+			Cell cell=new Cell().setTextAlignment(TextAlignment.LEFT).setHeight(20F);
+			table2.addCell(cell);
+		}
+		Cell c17 = new Cell(1,4);
+		c17.add(new Paragraph("英文、计算机水平").setFont(font).setBackgroundColor(Color.LIGHT_GRAY));
+		table2.addCell(c17);
+		Cell c18 = new Cell(2,4);
+		c18.add(new Paragraph("")).setFont(font).setHeight(40F);
+		table2.addCell(c18);
+		Cell c19 = new Cell(1,4);
+		c19.add(new Paragraph("奖励荣誉").setFont(font).setBackgroundColor(Color.LIGHT_GRAY));
+		table2.addCell(c19);
+		Cell c20 = new Cell(2,4);
+		c20.add(new Paragraph("")).setFont(font).setHeight(40F);
+		table2.addCell(c20);
+		Cell c21 = new Cell(1,4);
+		c21.add(new Paragraph("自我评价").setFont(font).setBackgroundColor(Color.LIGHT_GRAY));
+		table2.addCell(c21);
+		Cell c22 = new Cell(2,4);
+		c22.add(new Paragraph("")).setFont(font).setHeight(40F);
+		table2.addCell(c22);
+		doc.add(table);
+		doc.add(table2);
+		doc.close();
+	}
+	
+	
+}
